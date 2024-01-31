3b077d776b4a4b872d15e740fe186e9e1db05814
navbis <naveenbish>
2024-02-01 02:41:24 +0530

    Login and Signup pages added

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,15 +1,16 @@
 import {useNavigate} from 'react-router-dom';
+import {logo} from '../assets/index'
 
 const Signup = () => {
   const navigate = useNavigate()
   return (
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section class="bg-[#030303]">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
           class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-          DepressionHub
+         <img class="w-[300px] h-[100px] mr-2" src={logo} alt="logo" />
         </a>
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
